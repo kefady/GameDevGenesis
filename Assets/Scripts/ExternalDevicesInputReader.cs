3b3a322f2a84de733ee535db249d10bb0003c778
Kyryl Vashchuk <kirilvashchuk@gmail.com>
2023-03-29 17:08:29 +0300

    add ui input view

---
+++ Assets/Scripts/ExternalDevicesInputReader.cs
@@ -0,0 +1,28 @@
+﻿using Player;
+using UnityEngine;
+
+public class ExternalDevicesInputReader : IEntityInputSource
+{
+    public float HorizontalDirection => Input.GetAxisRaw("Horizontal");
+    public bool Jump { get; private set; }
+    public bool Flip { get; private set; }
+
+    public void OnUpdate()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump = true;
+        }
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Flip = true;
+        }
+    }
+
+    public void ResetOneTimeAction()
+    {
+        Jump = false;
+        Flip = false;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerBrain.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Player
+{
+    public class PlayerBrain
+    {
+        private readonly PlayerEntity _playerEntity;
+        private readonly List<IEntityInputSource> _inputSources;
+
+        public PlayerBrain(PlayerEntity playerEntity, List<IEntityInputSource> inputSources)
+        {
+            _playerEntity = playerEntity;
+            _inputSources = inputSources;
+        }
+
+        public void OnFixedUpdate()
+        {
+            _playerEntity.MoveHorizontally(GetHorizontalDirection());
+            if (IsJump()) _playerEntity.Jump();
+            foreach (var entityInputSource in _inputSources)
+            {
+                entityInputSource.ResetOneTimeAction();
+            }
+        }
+
+        private float GetHorizontalDirection()
+        {
+            foreach (var entityInputSource in _inputSources)
+            {
+                if (entityInputSource.HorizontalDirection == 0) continue;
+                return entityInputSource.HorizontalDirection;
+            }
+
+            return 0;
+        }
+
+        private bool IsJump() => _inputSources.Any(source => source.Jump);
+        private bool IsFlip() => _inputSources.Any(source => source.Flip);
+    }
+}
